989555e60f66978710c2a7575fadebdae1621a71
Coder-Lalit <coder.lalit@gmail.com>
2021-06-04 23:07:00 +0530

    stair case Fibonacci number

---
+++ src/LeetCode/Problems/Easy/climbStairs.java
@@ -0,0 +1,22 @@
+package LeetCode.Problems.Easy;
+
+public class climbStairs {
+    public static void main(String[] args) {
+        for(int i=1;i<12;i++){
+            System.out.println("Stair count :"+i+" Step Count :"+climbStairs(i));
+        }
+    }
+
+    public static int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+}

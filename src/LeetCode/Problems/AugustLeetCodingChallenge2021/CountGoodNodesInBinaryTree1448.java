224f7983c6b4d4a6f69159b925e9e53bef2b9bea
Coder-Lalit <coder.lalit@gmail.com>
2021-08-18 00:24:01 +0530

    some new answers

---
+++ src/LeetCode/Problems/AugustLeetCodingChallenge2021/CountGoodNodesInBinaryTree1448.java
@@ -0,0 +1,56 @@
+package LeetCode.Problems.AugustLeetCodingChallenge2021;
+
+import java.util.Stack;
+
+public class CountGoodNodesInBinaryTree1448 {
+    private static int count = 0;
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+
+    public static int goodNodes(TreeNode root) {
+        count=0;
+        dfs(root,Integer.MIN_VALUE);
+        return count;
+    }
+
+    static void dfs(TreeNode n, int max) {
+        if (n.val >= max) count++;
+        if (n.left != null) dfs(n.left, Integer.max(n.val, max));
+        if (n.right != null) dfs(n.right, Integer.max(n.val, max));
+    }
+
+    public static void main(String[] args) {
+        TreeNode n1 = new TreeNode(3, new TreeNode(1, new TreeNode(3), null), new TreeNode(4, new TreeNode(1), new TreeNode(5)));
+        TreeNode n2 = new TreeNode(3, new TreeNode(3, new TreeNode(4), new TreeNode(2)), null);
+        TreeNode n3 = new TreeNode(2, null, new TreeNode(4, new TreeNode(10), new TreeNode(8,new TreeNode(4),null)));
+        TreeNode n4 = new TreeNode(9, null, new TreeNode(3, new TreeNode(6), null));
+        TreeNode n5 = new TreeNode(2,
+                new TreeNode(4, new TreeNode(4), null)
+                , new TreeNode(4,
+                new TreeNode(1, new TreeNode(5, null, new TreeNode(5, new TreeNode(4), new TreeNode(4))), null),
+                new TreeNode(3)));
+        System.out.println(goodNodes(n1));
+        System.out.println(goodNodes(n2));
+        System.out.println(goodNodes(n3));
+        System.out.println(goodNodes(n4));
+        System.out.println(goodNodes(n5));
+
+    }
+}
--- src/LeetCode/Problems/Easy/Array/Shift2DGrid1260.java
+++ src/LeetCode/Problems/Easy/Array/Shift2DGrid1260.java
@@ -1,24 +1,69 @@
 package LeetCode.Problems.Easy.Array;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Shift2DGrid1260 {
+
+    //NotSolved
     public static void shiftGrid(int[][] grid, int k) {
-        k = k % grid[0].length;
+        int x = k % grid[0].length;
+        int y = k / grid.length;
+        int z = k % grid.length;
 
-        int order []= new int[grid[0].length];
-        order[0]= k;
-        for(int i=1;i<grid[0].length;i++){
-            order[i]=(order[i-1]+k)%grid[0].length;
-        }
+        int c[] = new int[grid[0].length];
+        int r[] = new int[grid.length];
+        int ans[][] = new int[grid.length][grid[0].length];
 
-        for (int x:order){
-            System.out.println(x);
+        System.out.println("==============================");
+        for (int i = 0; i < grid[0].length; i++) {
+            System.out.print((i + x) % grid[0].length + " ");
+            c[(i + x) % grid[0].length] = i;
         }
+        System.out.println("");
+        for (int i = 0; i < grid.length; i++) {
+            System.out.print((i + y) % grid.length + " ");
+            r[(i + y) % grid.length] = i;
+        }
+        System.out.println("\n==============================");
+
+
+        for (int i = 0; i < ans.length; i++) {
+            for (int j = 0; j < ans[0].length; j++) {
+                ans[i][j] = grid[r[i]][c[j]];
+            }
+        }
+        print(ans);
+
+
+        System.out.println("==============================");
+        for (int i = 0; i < grid.length; i++) {
+            System.out.print((i + y + z) % grid.length + " ");
+            r[(i + y + z) % grid.length] = i;
+        }
+        System.out.println("\n==============================");
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < z && j < grid[0].length; j++) {
+                ans[i][j] = grid[r[i]][c[j]];
+            }
+        }
+        print(ans);
+
     }
 
     public static void main(String[] args) {
-        shiftGrid(new int[][]{{3,8,1,9},{19,7,2,5},{4,6,11,10},{12,0,21,13}},4);
+        //shiftGrid(new int[][]{{1, 11, 111, 1111}, {2, 22, 222, 2222}, {3, 33, 333, 3333}, {4, 44, 444, 4444}}, 4);
+        //shiftGrid(new int[][]{{1, 11, 111, 1111}, {2, 22, 222, 2222}, {3, 33, 333, 3333}, {4, 44, 444, 4444}}, 6);
+        shiftGrid(new int[][]{{1}, {2}, {3}, {4}, {7}, {6}, {5}}, 23);
+        //shiftGrid(new int[][]{{1,2,3},{4,5,6},{7,8,9}},1);
+    }
+
+    static void print(int ans[][]) {
+        System.out.println("=============================");
+        for (int[] a : ans) {
+            for (int b : a) {
+                System.out.print(b + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("=============================");
     }
 }
